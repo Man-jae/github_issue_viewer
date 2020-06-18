3ebae52f91999409df9415e5986976d370005536
Minjae Kim <alswo5052@gmail.com>
2020-06-18 13:45:49 +0900

    네트워크 함수 추가

--- app/src/main/java/com/example/githubissueviewer/ui/MainActivity.kt
+++ app/src/main/java/com/example/githubissueviewer/ui/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.githubissueviewer.ui
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -43,6 +47,21 @@ class MainActivity : AppCompatActivity() {
 
     private fun updateRepoTitle(org: String, repo: String) {
         text_repo_title.text = getString(R.string.title, org, repo)
+    private fun checkNetwork(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val nw = connectivityManager.activeNetwork ?: return false
+            val actNw = connectivityManager.getNetworkCapabilities(nw) ?: return false
+            return when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            val nwInfo = connectivityManager.activeNetworkInfo ?: return false
+            return nwInfo.isConnected
+        }
+    }
     }
 
     private fun setIssueList(org: String, repo: String) {

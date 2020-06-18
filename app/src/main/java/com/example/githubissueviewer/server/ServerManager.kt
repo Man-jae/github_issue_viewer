ff762eaea4bea2c775806a621e3fec67d79d3861
Minjae Kim <alswo5052@gmail.com>
2020-06-18 13:37:05 +0900

    Issue, User class 이름 변경
    - Room 도입을 위한 서버 response 클래스로 변경

--- app/src/main/java/com/example/githubissueviewer/server/ServerManager.kt
+++ app/src/main/java/com/example/githubissueviewer/server/ServerManager.kt
@@ -1,13 +1,13 @@
 package com.example.githubissueviewer.server
 
-import com.example.githubissueviewer.data.Issue
+import com.example.githubissueviewer.data.IssueResponse
 import retrofit2.Response
 
 class ServerManager {
     companion object {
         private val SERVER = ServerDefine.retrofit.create(ServerInterface::class.java)
 
-        suspend fun getIssueList(org: String, repo: String): Response<ArrayList<Issue>> {
+        suspend fun getIssueList(org: String, repo: String): Response<ArrayList<IssueResponse>> {
             return SERVER.getIssueList(org, repo)
         }
     }
